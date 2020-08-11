bcc20504f2b3c8fdf07258212045b94cc2517861
kolya <nik.yavetskiy@gmail.com>
2020-08-11 19:53:25 +0300

    navbar and image

--- index.php
+++ index.php
@@ -10,6 +10,24 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="#">NIKO</a>
+
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <ul class="navbar-nav mr-auto mt-2 mt-md-0">
+            <li class="nav-item active">
+                <a class="nav-link" href="main_page.html">Головна сторінка <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="C:\OSPanel\domains\localhost\webform\index.php">Форма</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
     <div class="content">
         <button class="show_popup blue_btn" rel="popup1">Залишити заявку</button>
     </div>
